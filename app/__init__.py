16b3c915f100c1b89a297b7180434a0efff30838
codesungrape <miss.s.rai@outlook.com>
2025-06-23 15:03:51 +0100

    Fix pylint import warning by replacing wrong-import-position with import-outside-toplevel

--- app/__init__.py
+++ app/__init__.py
@@ -16,7 +16,7 @@ def create_app():
     app.config['COLLECTION_NAME'] = os.getenv('PROJECT_COLLECTION')
 
     # Import routes — routes can import app safely because it exists
-    from app.routes import register_routes # pylint: disable=wrong-import-position
+    from app.routes import register_routes # pylint: disable=import-outside-toplevel
     register_routes(app)
 
     return app
