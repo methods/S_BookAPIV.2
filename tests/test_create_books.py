574be79f17d07bd36c614ffb0e1eeaaaae9b206b
codesungrape <miss.s.rai@outlook.com>
2025-06-25 16:30:18 +0100

    Add mongomock test and fixture for populate_books function

--- tests/test_create_books.py
+++ tests/test_create_books.py
@@ -1,8 +1,58 @@
-# pylint: disable=missing-docstring
+# pylint: disable=missing-docstring,line-too-long
+import mongomock
+import pytest
 
-# this test should check resource is in db
-# resource that it will be adding is the resource from sample_books.json
-# to import .json file i need to ...?
-# it is also an integration test so needs fixtures at the top
-# check the integration test file form before
+@pytest.fixture(name="mock_books_collection")
+def mock_books_collection_fixture():
+    """Provides an in-memory, empty 'books' collection for each test."""
+    # mongomock.MongoClient() creates a fake client.
+    client = mongomock.MongoClient()
+    db = client['test_database']
+    return db["test_books_collection"]
 
+def test_populate_books_inserts_data_to_db(mock_books_collection):
+
+    # Arrange
+    test_books = [
+            {
+            "id": "550e8400-e29b-41d4-a716-446655440000",
+            "title": "To Kill a Mockingbird",
+            "synopsis": "The story of racial injustice and the loss of innocence in the American South.",
+            "author": "Harper Lee",
+            "links": {
+                "self": "/books/550e8400-e29b-41d4-a716-446655440000",
+                "reservations": "/books/550e8400-e29b-41d4-a716-446655440000/reservations",
+                "reviews": "/books/550e8400-e29b-41d4-a716-446655440000/reviews"
+                },
+            "state": "active"
+        },
+        {
+            "id": "550e8400-e29b-41d4-a716-446655440001",
+            "title": "1984",
+            "synopsis": "A dystopian novel about totalitarianism and surveillance.",
+            "author": "George Orwell",
+            "links": {
+                "self": "/books/550e8400-e29b-41d4-a716-446655440001",
+                "reservations": "/books/550e8400-e29b-41d4-a716-446655440001/reservations",
+                "reviews": "/books/550e8400-e29b-41d4-a716-446655440001/reviews"
+                },
+            "state": "active"
+        }
+    ]
+    expected_book_count = len(test_books)
+
+    # Act
+    # Inject our mockbooks collection and test_data into the function
+    result = populate_books(mock_books_collection, test_books)
+
+    # 1. Assert - function's return value for immediate feedback
+    assert result.upserted_count == expected_book_count
+    assert result.matched_count == 0
+
+    # Assert - final state of the db
+    assert mock_books_collection.count_documents({}) == expected_book_count
+
+    # 3. Assert - spot-check acutal content
+    retrieved_book = mock_books_collection.find_one({"title": "1984"})
+    assert retrieved_book is not None
+    assert retrieved_book["author"] == "George Orwell"
