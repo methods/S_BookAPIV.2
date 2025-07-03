50c5402406b2e06166afe3507321e4230d8a8cd4
codesungrape <miss.s.rai@outlook.com>
2025-07-03 10:42:55 +0100

    Refactor integration test and add isolated unit test using mock data

--- tests/test_db_helpers.py
+++ tests/test_db_helpers.py
@@ -4,11 +4,48 @@ import json
 import pytest
 from utils.db_helpers import load_books_json
 
-def test_can_load_books_json():
-    books = load_books_json()
-    print(books)
-    assert isinstance(books, list)
-    assert "title" in books[0]
+def test_load_books_json_successfully():
+
+    # Arrange
+    test_books_data = '''[
+        {
+            "id": "550e8400-e29b-41d4-a716-446655440003",
+            "title": "Pride and Prejudice",
+            "synopsis": "A romantic novel about the relationship between Elizabeth Bennet and Mr. Darcy.",
+            "author": "Jane Austen",
+            "links": {
+            "self": "/books/550e8400-e29b-41d4-a716-446655440003",
+            "reservations": "/books/550e8400-e29b-41d4-a716-446655440003/reservations",
+            "reviews": "/books/550e8400-e29b-41d4-a716-446655440003/reviews"
+            },
+            "state": "active"
+        },
+        {
+            "id": "550e8400-e29b-41d4-a716-446655440004",
+            "title": "The Catcher in the Rye",
+            "synopsis": "A story about teenage rebellion and alienation.",
+            "author": "J.D. Salinger",
+            "links": {
+            "self": "/books/550e8400-e29b-41d4-a716-446655440004",
+            "reservations": "/books/550e8400-e29b-41d4-a716-446655440004/reservations",
+            "reviews": "/books/550e8400-e29b-41d4-a716-446655440004/reviews"
+            },
+            "state": "active"
+        }
+    ]'''
+    # use mock_open to simulate reading this valid content
+    mocked_file = mock_open(read_data=test_books_data)
+
+    with patch("builtins.open", mocked_file):
+
+        # Act
+        books = load_books_json()
+        # Assert
+        assert isinstance(books, list)
+        assert len(books) == 2
+
+        assert books[0]["title"] == "Pride and Prejudice"
+        assert books[-1]["title"] == "The Catcher in the Rye"
 
 def test_load_books_raises_error_if_file_not_found():
     # Arrange:
@@ -31,10 +68,10 @@ def test_load_books_raises_decodeerror_for_invalid_json(capsys):
     '''
 
     # Use mock_open to simulate reading our broken JSON string
-    # The read_data parameter pre-populates the content that 'read()' will return
     m = mock_open(read_data=mock_file_content)
 
     # Patch 'open' used in the module under test
+    # When 'open()' is called, use this fake file
     with patch("builtins.open", m):
         # Act and Assert
         with pytest.raises(json.JSONDecodeError):
@@ -42,3 +79,11 @@ def test_load_books_raises_decodeerror_for_invalid_json(capsys):
 
     captured = capsys.readouterr()
     assert "Failed to decode JSON" in captured.out
+
+
+def test_integration_load_books_json():
+    books = load_books_json()
+
+    assert isinstance(books, list)
+    assert isinstance(books[0], dict)
+    assert "title" in books[0]
