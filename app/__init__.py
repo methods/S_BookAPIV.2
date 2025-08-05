9885558118e260644f5066837763729b15a833c2
codesungrape <miss.s.rai@outlook.com>
2025-08-05 17:48:09 +0100

    add Flask-PyMongo extension and init in create_app factory

--- app/__init__.py
+++ app/__init__.py
@@ -3,9 +3,14 @@
 import os
 
 from flask import Flask
+from flask_pymongo import PyMongo
 
 from app.config import Config
 
+# Create PyMongo extension object outside the factory
+# in the global scope, creating an "empty" extension object
+# This way, we can import it in other files
+mongo = PyMongo()
 
 def create_app(test_config=None):
     """Application factory pattern."""
@@ -18,7 +23,10 @@ def create_app(test_config=None):
     if test_config:  # Override with test specifics
         app.config.from_mapping(test_config)
 
-    # Import routes
+    # Connect Pymongo to our specific app instance
+    mongo.init_app(app)
+
+    # Import and register routes
     from app.routes import \
         register_routes  # pylint: disable=import-outside-toplevel
 
