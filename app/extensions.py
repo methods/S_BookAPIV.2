4fe49bd1f22c1d65b41eafcd50d6eb08f03b5d42
codesungrape <miss.s.rai@outlook.com>
2025-08-14 14:53:57 +0100

    Install and initialize Flask-Bcrypt globally in app

--- app/extensions.py
+++ app/extensions.py
@@ -1,7 +1,9 @@
 """Module for Flask extensions."""
 
 from flask_pymongo import PyMongo
+from flask_bcrypt import Bcrypt
 
 # Createempty PyMongo extension object globally
 # This way, we can import it in other files and avoid a code smell: tighly-coupled, cyclic error
 mongo = PyMongo()
+bcrypt = Bcrypt()
