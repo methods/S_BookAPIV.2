c83cfb213dd5ded82a75532000cadf82f07a5bc1
codesungrape <miss.s.rai@outlook.com>
2025-09-15 16:13:41 +0100

    Update tests to include sort in chain of mock calls

--- tests/test_book_service.py
+++ tests/test_book_service.py
@@ -32,7 +32,7 @@ def test_fetch_active_books_uses_default_pagination(mock_mongo, test_app):
     WHEN fetch_active_books is called
     THEN it should query the database using the default offset (0) and limit (20).
     """
-    mock_mongo.db.books.find.return_value.skip.return_value.limit.return_value = [
+    mock_mongo.db.books.find.return_value.sort.return_value.skip.return_value.limit.return_value = [
         {"_id": "1", "title": "A Book"}
     ]
     with test_app.app_context():
@@ -47,10 +47,8 @@ def test_fetch_active_books_uses_default_pagination(mock_mongo, test_app):
     # 2. Check that the database methods were called with the correct default values
     expected_filter = {"state": {"$ne": "deleted"}}
     mock_mongo.db.books.find.assert_called_once_with(expected_filter)
-    mock_mongo.db.books.find.return_value.skip.assert_called_once_with(0)
-    mock_mongo.db.books.find.return_value.skip.return_value.limit.assert_called_once_with(
-        20
-    )
+    mock_mongo.db.books.find.return_value.sort.return_value.skip.assert_called_once_with(0)
+    mock_mongo.db.books.find.return_value.sort.return_value.skip.return_value.limit.assert_called_once_with(20) #pylint: disable=line-too-long
 
 
 @patch("app.services.book_service.mongo")
@@ -71,7 +69,5 @@ def test_fetch_active_books_uses_custom_pagination(mock_mongo, test_app):
     # Check that the database methods were called with the custom values
     expected_filter = {"state": {"$ne": "deleted"}}
     mock_mongo.db.books.find.assert_called_once_with(expected_filter)
-    mock_mongo.db.books.find.return_value.skip.assert_called_once_with(10)
-    mock_mongo.db.books.find.return_value.skip.return_value.limit.assert_called_once_with(
-        5
-    )
+    mock_mongo.db.books.find.return_value.sort.return_value.skip.assert_called_once_with(10)
+    mock_mongo.db.books.find.return_value.sort.return_value.skip.return_value.limit.assert_called_once_with(5) # pylint: disable=line-too-long
